a5be792f68e178626fc2c90b7262bda77495a2cd
Carlos <Carlos@Elite>
2015-07-26 02:01:32 -0500

    Asterisk Interfaz

--- src/asterisk/Asterisk.java
+++ src/asterisk/Asterisk.java
@@ -41,98 +41,22 @@ public class Asterisk {
         }
     }
     
-    public void CrearTermino(){
-        
+
+    
+    public void menu(){
+    
+        System.out.println("\t\tMenu");
+        System.out.println("1.Crear Tema");
+        System.out.println("2.Crear Titulo");
+        System.out.println("3.Crear Termino");
+        System.out.println("4.Crear Relacion");
+        System.out.println("5.Ver Temas");
+        System.out.println("6.Ver ");
+        System.out.println("1.Crear Tema");
     }
-
     public static void main(String[] args) {
-
-        /*      Scanner scan = new Scanner(System.in);
-         AnsiConsole.systemInstall();
-
-         System.out.println(ansi().fg(Color.YELLOW).a("Hello World").reset());
-         System.out.println(ansi().fg(Color.BLUE).a("Hello World").reset());
-
-         System.out.println(ansi().fg(Color.GREEN).a("Hello World").reset());
-         */
-// System.out.println(ansi().eraseScreen().render("@|red Hello|@ @|green World|@"));
-        //System.err.println(ansi().eraseScreen());
-        //  AnsiConsole.systemUninstall();
-        //scan.next();
-        //System.out.println("");
-//        DAO dao = new DAO();
-//        String termino;
-//        int temaId;
-//        int tituloId;
-//        int option;
-//
-//        ArrayList<Tema> temas = dao.ListaTemas();
-//        clearConsole();
-//        System.out.println("Lista Temas: ");
-//        for (Tema tema : temas) {
-//            System.out.println(tema.toString());
-//        }
-//        
-//        
-//
-//        System.out.println("----------------------------");
-//        System.out.println("Tema ID:");
-//        temaId = new Integer(scan.nextLine());
-//        while (true) {
-//            System.out.println("1. Ingresar\n2. Leer\n3. Temas\n4. Terminos Tema");
-//            option = new Integer(scan.nextLine());
-//
-//            switch (option) {
-//                case 1:
-//                    while (true) {
-//                        Termino t = new Termino();
-//                        Tema tema = new Tema();
-//                        System.out.println("Termino: ");
-//                        termino = scan.nextLine();
-//                        if (termino.equals("")) {
-//                            break;
-//                        }
-//                        t.setTermino(termino);
-//                        tema.setId(temaId);
-//                        dao.create(t);
-//                        clearConsole();
-//                    }
-//                    break;
-//                case 2:
-//                    while (true) {
-//                        Termino tt = dao.darTermino();
-//                        System.out.println(tt.toString());
-//                        String k = scan.nextLine();
-//                        if (!k.equals("")) {
-//                            break;
-//                        }
-//                    }
-//                    break;
-//                case 3:
-//                    System.out.println("Lista Temas: ");
-//                    for (Tema tema : temas) {
-//                        System.out.println(tema.toString());
-//                    }
-//                    break;
-//
-//                case 4:
-//                    System.out.println("Lista Temas: ");
-//                    for (Tema tema : temas) {
-//                        System.out.println(tema.toString());
-//                    }
-//                    temaId = new Integer(scan.nextLine());
-//                    ArrayList<Termino> teminos = dao.ListaTerminosTema(temaId);
-//                    clearConsole();
-//                    int i = 1;
-//                    for (Termino t : teminos) {
-//                        System.out.println(" -" + (i < 10 ? "  " : " ") + i + ". " + t.toString());
-//                        i++;
-//                    }
-//
-//                    break;
-//            }
-//
-//        }
+        
+    
     }
 
 }
