f413f8cb0984d2f8c43605659aca0103de0812bf
Carlos <Carlos@Elite>
2015-07-26 23:43:16 -0500

    Asterisk 80%

---
+++ src/asterisk/QuickAsterisk.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package asterisk;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Carlos
+ */
+public class QuickAsterisk {
+
+    private static Scanner scan = new Scanner(System.in);
+    
+    public static void interprete(){
+        
+    }
+    public static void main(String[] args) {
+        
+    }
+}
